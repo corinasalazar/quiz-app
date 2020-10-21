17b70faabfc0c09013871bdd8f57bfec4e3bebd5
Christopher Wylie <69485943+Christopher-Wylie@users.noreply.github.com>
2020-10-21 11:07:32 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const store = {
+    questions: [
+       { question: "How many members were in Kill Bill's Deadly Viper Assassination Squad?",
+        answers: ["nine", "ten", "six", "seventeen"], 
+        correctAnswer: "six"},
+
+        {question: "Who snapped the Infinity Gaunlet at the end of Avengers: Infinity War?",
+        answers: ["Thanos", "Ironman", "Hulk", "Spider-man"],
+        correctAnswer: "Thanos"},
+
+        {question: "When was the first Rambo movie released?",
+        answers: ["1940", "1973", "1768", "1982"],
+        correctAnswer: "1982"},
+
+        {question: " How many movies make up the Fast & Furious franchise so far?",
+        answers: ["nine", "ten", "seven", "twenty four"],
+        correctAnswer: "nine"},
+
+        {question: "who played ryan in saving private ryan answer?",
+        answers: ["Matt Damon", "Tom Hardy", "Tom Hanks", "Paul Rudd"],
+        correctAnswer: "Matt Damon"},
+    ],
+    quizStarted: false,
+    questionNumber: 0,
+    score: 0,
+};
+
+function renderQuiz(){
+    let renderQuiz = 
+}
+// quiz start page should render
+
+// you should be able to start the quiz by pressing a start button
+
+//you should be shown a question with a list of answers
+
+//users should be able to select an answer
+
+//app should verify whether answer is correct
+
+//when correct, user will be taken to a correct answer page with the correct answer highlighted green, and 1 will be added to total number correct
+
+//when incorrect, user will be taken to a incorrect answer page with the correct answer highlighted green and the chosen answer highlighted red
+
+//user should be able to proceed to next question with a next question button
+
+//once quiz is complete user will be displayed a final page stating that the quiz is over and will also display final score 
+
+//A button to restart the quiz will be displayed on final page
